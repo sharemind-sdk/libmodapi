88450ed399c875528f03a0f04f483d1e3819c635
Jaak Ristioja <jaak.ristioja@cyber.ee>
2015-06-18 13:10:23 +0300

    Integrated apis.c into module.c

--- src/module.c
+++ src/module.c
@@ -25,11 +25,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include "modapi.h"
+#include "module_0x1.h"
+#include "pd_0x1.h"
+#include "pdpi_0x1.h"
 
 
-// Include API list:
-#include "apis.c"
-
+static SharemindApi const SharemindApis[] = {
+    { .moduleLoad = &SharemindModule_load_0x1,
+      .moduleUnload = &SharemindModule_unload_0x1,
+      .moduleInit = &SharemindModule_init_0x1,
+      .moduleDeinit = &SharemindModule_deinit_0x1,
+      .numSyscalls = &SharemindModule_numSyscalls_0x1,
+      .syscall = &SharemindModule_syscall_0x1,
+      .findSyscall = &SharemindModule_findSyscall_0x1,
+      .numPdks = &SharemindModule_numPdks_0x1,
+      .pdk = &SharemindModule_pdk_0x1,
+      .findPdk = &SharemindModule_findPdk_0x1,
+      .numPds = &SharemindModule_numPds_0x1,
+      .pd = &SharemindModule_pd_0x1,
+      .findPd = &SharemindModule_findPd_0x1,
+      .startPd = &SharemindPd_start_0x1,
+      .stopPd = &SharemindPd_stop_0x1,
+      .startPdpi = &SharemindPdpi_start_0x1,
+      .stopPdpi = &SharemindPdpi_stop_0x1
+    }
+};
 
 SharemindModule * SharemindModuleApi_newModule(SharemindModuleApi * modapi,
                                                const char * filename,
